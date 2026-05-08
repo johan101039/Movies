ad3c7f723177ca3ad1196cb7b4c5c2d213fca11f
Johan <johanmendozatapia01@gmail.com>
2026-05-08 12:23:29 -0500

    fix api typing

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,26 +1,26 @@
-import type { Data } from "../interfaces/Movie";
+import type { Data, Movies } from "../interfaces/Movie";
 
 const API_KEY = "22d8c17988d5590d8b2dc391f908e9ea";
 const BASE_URL = "https://api.themoviedb.org/3";
 
-// Películas populares
+// 🎬 Películas populares
 export const getMovies = async (): Promise<Data> => {
-    const response = await fetch(
-        `${BASE_URL}/movie/popular?api_key=${API_KEY}`
-    );
+  const response = await fetch(
+    `${BASE_URL}/movie/popular?api_key=${API_KEY}`
+  );
 
-    const data: Data = await response.json();
-    console.log(data.results);
-    return data;
+  const data: Data = await response.json();
+
+  return data;
 };
 
-// Buscar películas
-export const searchMovies = async (query: string) => {
-    const response = await fetch(
-        `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`
-    );
+// 🔍 Buscar películas (TIPADO CORRECTO)
+export const searchMovies = async (query: string): Promise<Movies[]> => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`
+  );
 
-    const data = await response.json();
+  const data: Data = await response.json();
 
-    return data.results;
+  return data.results;
 };
